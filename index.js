f28a29e81a4ab5e7bfdc172a9aedf0c8d7d670ab
Stephan Griesel <sgriesel@gmail.com>
2018-09-27 06:39:34 +0200

    prepare array

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const brain = require('brain.js');
 
 const network = new brain.NeuralNetwork();
+
+network.train([
+    { input: [], output: [] },
+    { input: [], output: [] },
+    { input: [], output: [] },
+    { input: [], output: [] },
+    { input: [], output: [] }
+]);
